5adc4acf3f340e8dfcbaa41874cd677bd366ebbd
Marco Ruiter <mruiter@tuxweb.nl>
2025-08-08 10:49:48 +0200

    chore: add pairing debug logs

--- drivers/wifipool/pair/list_devices.js
+++ drivers/wifipool/pair/list_devices.js
@@ -1,19 +1,26 @@
 Homey.on('init', () => {
+  console.log('Pairing: view initialized, requesting device list');
   Homey.emit('list_devices', {}, (err, devices) => {
     if (err) {
+      console.error('Pairing: error retrieving devices', err);
       return Homey.alert(err);
     }
 
+    console.log('Pairing: received devices', devices);
     const list = document.getElementById('devices');
     devices.forEach(device => {
       const li = document.createElement('li');
       const btn = document.createElement('button');
       btn.textContent = device.name;
       btn.addEventListener('click', () => {
+        console.log('Pairing: creating device', device);
         Homey.createDevice(device, (createErr) => {
           if (createErr) {
+            console.error('Pairing: error creating device', createErr);
             return Homey.alert(createErr);
           }
+
+          console.log('Pairing: device created successfully');
         });
       });
       li.appendChild(btn);
--- drivers/wifipool/pair/view.js
+++ drivers/wifipool/pair/view.js
@@ -1,6 +1,8 @@
 Homey.on('init', () => {
+  console.log('Pairing: start view initialized');
   const nextBtn = document.getElementById('next');
   nextBtn.addEventListener('click', () => {
+    console.log('Pairing: next button clicked, showing device list');
     Homey.showView('list_devices');
   });
 });
