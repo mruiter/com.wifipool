5eaefa340eb745a042ae3e55d77d429040547384
Marco Ruiter <mruiter@tuxweb.nl>
2025-08-14 19:12:36 +0200

    Add list_devices pairing handler

--- drivers/wifipool/pair/start.js
+++ drivers/wifipool/pair/start.js
@@ -1,5 +1,17 @@
 export default class {
   onPair(socket) {
+    socket.on('list_devices', async (data, callback) => {
+      try {
+        const devices = [{
+          name: 'WiFi Pool',
+          data: { id: 'wifipool-1' }
+        }];
+        callback(null, devices);
+      } catch (err) {
+        callback(err);
+      }
+    });
+
     socket.on('add_device', async (data, callback) => {
       try {
         const device = {
